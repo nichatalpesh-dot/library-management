119760a137768164f2fce43fa3589b3636f6f1a5
nichatalpesh-dot <nichatalpesh@gmail.com>
2026-03-13 10:42:05 +0530

    second commit

--- library-management/src/Countor.jsx
+++ library-management/src/Countor.jsx
@@ -8,6 +8,7 @@ function Counter() {
       <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
